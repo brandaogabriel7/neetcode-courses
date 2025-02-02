a5942f5c6f392a66216f06f161147833ccc5c578
Gabriel Brandão <gabriel.brandao@simplelabtech.com.br>
2025-02-02 01:26:21 +0000

    valid anagram solution (sorted approach)

--- roadmap-exercises/valid-anagram.py
+++ roadmap-exercises/valid-anagram.py
@@ -3,11 +3,4 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        hash_s = {}
-        hash_t = {}
-
-        for i in range(len(s)):
-            hash_s[s[i]] = 0 if s[i] not in hash_s else hash_s[s[i]] + 1
-            hash_t[t[i]] = 0 if t[i] not in hash_t else hash_t[t[i]] + 1
-
-        return hash_s == hash_t
+        return sorted(s) == sorted(t)
